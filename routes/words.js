141a98056b0b85b07e88cc7dba81400065faf07f
Monica Yang <1212monicayang@gmail.com>
2026-02-18 12:20:57 -0500

    Wotd (w Error)

--- routes/words.js
+++ routes/words.js
@@ -7,8 +7,25 @@ readFile
 router.get('/', (req,res) =>{
     res.send ('Word Home Page');
 });
-router.get('/wotd', (req,res) =>{
-    res.render('wotd');
+router.get('/wotd', async (req,res) =>{
+    let wordArray = await getWordFromDictionary();
+    let [word, part, def] = wordArray;
+    res.render('wotd', {word:word, part:part, def:def});
 });
 
 module.exports = router;
+
+let getWordFromDictionary = async () =>{
+    try{
+        const data = await readFile('resources/allwords.txt', 'utf8');
+        let lines = data.split('\n');
+        let ranNum = parseIn(Math.random() * lines.length);
+        let ranLine = lines[ranNum];
+        let wordArray = ranLine.split('\t');
+        console.log(wordArray);
+        return wordArray;
+    }
+    catch(err){
+        console.log("There was an error: ", err);
+    }
+}
